e611c4e090614eda27887ed50be2e9c5815ede90
Sean Maraia <selio@me.com>
2015-11-08 02:51:18 -0500

    Revert "Revert "Added Functionality changing appearance of tiles based on TileHeight, showing Characters on the tiles, and a rudimentary (and buggy) UI. Current Problems: The canvas should clear whenever a button is clicked on the UI, but this doesn't happen the first time a player clicks a button. In addition, Possible Moves are shown only at initialization, since the map cannot be accessed any time other than the InitMap function""
    This reverts commit cf569df5e933ff862802d2c10371858617c679e8.

--- SuperHeroesUnity/Assets/UIManager.cs
+++ SuperHeroesUnity/Assets/UIManager.cs
@@ -1,41 +1,156 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+enum UIState
+{
+    Default,
+    Movement,
+    Attacking
+}
 
 public class UIManager : MonoBehaviour {
     
     GridHandler map;
-
+    UIState currentState;
+    GameObject CurrentCanvas;
+    Stack History = new Stack();
+    GameObject[,] Tiles;
+    List<GameObject> Characters;
+    public List<Node> Moves;
 	// Use this for initialization
 	void Start () {
+        currentState = UIState.Default;
+        UIUpdate();
+        Characters = new List<GameObject>();
         
-        map = gameObject.GetComponent<GameManager>().map;
+	}
 
-        Texture2D tex = new Texture2D(1, 1, TextureFormat.ARGB32, false);
-        tex.SetPixel(0, 0, Color.white);
-        tex.Resize(100, 100);
-        tex.Apply();
+    public void InitMap(GridHandler givenMap)
+    {
+        
+        map = givenMap;
+        Tiles = new GameObject[map.Height,map.Width];
+
 
         for (int y = 0; y < map.Height; y++)
         {
-            for(int x = 0; x < map.Width; x++)
+            for (int x = 0; x < map.Width; x++)
             {
-                Debug.Log("run # " + x);
                 GameObject tempTile = Instantiate(Resources.Load("tile")) as GameObject;
-                //tempTile.bounds.SetMinMax(new Vector3(x * 32, y * 32, -10), new Vector3(x * 32 + 32, y * 32 + 32, -10));
-                tempTile.transform.position = new Vector3(x - 5 , y - 4.5f , 2);
+                tempTile.transform.position = new Vector3(x - 5, y - 4.5f, 0);
                 tempTile.GetComponent<TileScript>().x = x;
                 tempTile.GetComponent<TileScript>().y = y;
+                TerrainHeight tileHeight = map.map[y, x].height;
+                switch (tileHeight)
+                {
+                    case TerrainHeight.Empty:
+                        tempTile.GetComponent<SpriteRenderer>().color = Color.black;
+                        break;
+                    case TerrainHeight.Ground:
+                        tempTile.GetComponent<SpriteRenderer>().color = Color.white;
+                        break;
+                    case TerrainHeight.Wall:
+                        tempTile.GetComponent<SpriteRenderer>().color = Color.gray;
+                        break;
+                }
+
+                Tiles[y, x] = tempTile;
+
+                if (map.map[y, x].myCharacter != null)
+                {
+                    GameObject character = Instantiate(Resources.Load("Character")) as GameObject;
+                    character.transform.position = new Vector3(x - 5, y - 4.5f, -0.5f);
+                    if (map.map[y, x].myCharacter.Faction == 0)
+                    {
+                        character.GetComponent<SpriteRenderer>().color = Color.green;
+                        Moves = map.map[y, x].FindPossibleMoves(0, 4, null, null);
+                    } else character.GetComponent<SpriteRenderer>().color = Color.red;
+
+                    Characters.Add(character);
+                }
             }
         }
-	}
+
+        foreach (Node node in Moves)
+        {
+            Tiles[node.Y, node.X].GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
 
     public void TileClicked(int x, int y)
     {
         Debug.Log("The Tile at " + x + ", " + y + " is " + map.map[y, x].height);
     }
+
+   public void ButtonClicked(string buttonState)
+    {
+        History.Push(currentState);
+        clearUI();        
+        switch (buttonState)
+        {
+            case "Movement":
+                currentState = UIState.Movement;
+                break;
+            case "Attack":
+                currentState = UIState.Attacking;
+                break;
+            default:
+                currentState = UIState.Default;
+                break;
+        }
+        UIUpdate();
+    }
+
+    void UIUpdate()
+    {
+        switch (currentState)
+        {
+            case UIState.Movement:
+                drawMovementUI();
+                break;
+            case UIState.Attacking:
+                drawAttackingUI();
+                break;
+            default:
+                drawDefaultUI();
+                break;
+        }
+    }
 	
+    void clearUI()
+    {
+        Debug.Log("Clearing UI");
+
+        Destroy(CurrentCanvas.gameObject);
+    }
+
+    void drawDefaultUI()
+    {
+        Debug.Log("Default? " + currentState);
+        CurrentCanvas = Instantiate(Resources.Load("UI/DefaultCanvas")) as GameObject;
+    }
+
+    void drawMovementUI()
+    {
+        Debug.Log("Moving? " + currentState);
+        CurrentCanvas = Instantiate(Resources.Load("UI/MovementCanvas")) as GameObject;
+
+        
+        
+    }
+
+    void drawAttackingUI()
+    {
+        Debug.Log("Attacking? " + currentState);
+        CurrentCanvas = Instantiate(Resources.Load("UI/AttackingCanvas")) as GameObject;
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        if(Input.GetKey(KeyCode.Space) == true)
+        {
+            clearUI();
+        }
 	}
 }
