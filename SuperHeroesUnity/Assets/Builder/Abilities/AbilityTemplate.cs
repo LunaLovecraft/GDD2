edb433f8a41ccac9888703093c6bc1ec9d9bf9f0
LunaLovecraft <lunalovecraft@gmail.com>
2015-11-16 17:01:24 -0500

    Added in the ability to add information to abilities.

--- SuperHeroesUnity/Assets/Builder/Abilities/AbilityTemplate.cs
+++ SuperHeroesUnity/Assets/Builder/Abilities/AbilityTemplate.cs
@@ -9,6 +9,20 @@ public static partial class Abilities
 {
 
     /// <summary>
+    /// Range info
+    /// </summary>
+    public static int ExampleRange = 0;
+
+    public static int ExampleSplash = 0;
+
+    /// <summary>
+    /// Offensive info
+    /// </summary>
+    public static bool ExampleOffensive = true;
+
+   
+
+    /// <summary>
     /// Example Ability Implementation.  All ablities must be part of the static partial class Abilities.
     /// </summary>
     public static void Example(Character myChar, GridHandler map)
@@ -17,7 +31,7 @@ public static partial class Abilities
         choices.Add(new List<Node>());
         choices[0].Add(myChar.MyLocation);
 
-        //UIInformationHandler.InformationStack.Push(new UIInformation(myChar, map, choices, 1, ExamplePartTwo));
+        UIInformationHandler.InformationStack.Push(new UIInformation(myChar, map, choices, 1, ExamplePartTwo));
     }
 
     public static void ExamplePartTwo(Character myChar, GridHandler map, int affectedFaction, List<Node> affectedNodes)
