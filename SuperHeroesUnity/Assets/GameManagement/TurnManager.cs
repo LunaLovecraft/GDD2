0ef880e57a7664dbe90b57a60c07b24d0e263f26
unknown <mtc5284@GDD-10.main.ad.rit.edu>
2015-11-23 14:57:43 -0500

    Added back in the Turn Manager

---
+++ SuperHeroesUnity/Assets/GameManagement/TurnManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum TurnState
+{
+    TurnStart,
+    Actions,
+    TurnEnd
+}
+
+public class TurnManager : MonoBehaviour
+{
+    TurnOrderManager turnOrderManager;
+    Character character;
+    TurnState turnState;
+
+    UIManager uIManager;
+
+    void Start()
+    {
+        turnOrderManager = GetComponent<TurnOrderManager>();
+        turnState = TurnState.TurnStart;
+        uIManager = GetComponent<UIManager>();
+    }
+
+    public void StartNewTurn(Character character)
+    {
+        this.character = character;
+    }
+
+    private void TurnStart()
+    {
+    }
+
+    private void TurnAction()
+    {
+    }
+
+    public void TurnEnd()
+    {
+        turnOrderManager.EndCurrentTurn();
+    }
+}
---
+++ SuperHeroesUnity/Assets/GameManagement/TurnOrderManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurnOrderManager : MonoBehaviour
+{
+    public int currentTurn = 0;
+    public List<Character> turnOrder  = new List<Character>();
+    public TurnManager turnManager;
+
+    void Start()
+    {
+        turnManager = gameObject.AddComponent<TurnManager>();
+    }
+
+    public void StartTurnOrder()
+    {
+        turnManager.StartNewTurn(turnOrder[currentTurn]);
+    }
+
+
+    /// <summary>
+    /// Add character to turn order.
+    /// </summary>
+    /// <param name="character">The character to be added</param>
+    public void AddToTurnOrder(Character character)
+    {
+        Debug.Log(character + ": " + character.Name);
+        turnOrder.Add(character);
+    }
+
+    public void RemoveFromTurnOrder(Character character)
+    {
+        int index = FindOnTurnOrder(character);
+        turnOrder.RemoveAt(index);
+
+        if (index < currentTurn)
+            currentTurn--;
+    }
+
+    public int FindOnTurnOrder(Character character)
+    {
+        int index = turnOrder.BinarySearch(character);
+
+        if (index >= 0)
+            return index;
+        else
+            return -1;
+    }
+
+    public Character GetCurrentTurnCharacter()
+    {
+        return turnOrder[currentTurn];
+    }
+
+    public int GetCurrentTurnNumber()
+    {
+        return currentTurn;
+    }
+
+    public void EndCurrentTurn()
+    {
+        currentTurn = (currentTurn + 1) % turnOrder.Count;
+
+        turnManager.StartNewTurn(turnOrder[currentTurn]);
+    }
+}
